0d0b3e045258139e43548032d255ba008497f37e
EwDosbox <144440258+EwDosbox@users.noreply.github.com>
2026-05-15 23:42:03 +0200

    cheatshest notif turned off

--- sway/.config/sway/scripts/cheatsheet_hint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Send a notification about the keybinding cheatsheet
-notify-send -t 600000 -i $HOME/.icons/up-left.png "  Click the keyboard icon in the Waybar to launch the Sway keybinding cheatsheet."
-
-# Self-destructing script!
-rm $HOME/.config/sway/config.d/cheatsheet_hint
-rm -- "$0"
